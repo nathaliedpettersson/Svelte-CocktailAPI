08e9230e83a130da101e05ed4acd57bfe05e3dd4
nathaliedpettersson <nathalie.pettersson90@gmail.com>
2023-03-11 17:19:03 +0100

    add: gh-pages branch

--- .svelte-kit/output/server/manifest.js
+++ .svelte-kit/output/server/manifest.js
@@ -1,7 +1,7 @@
 export const manifest = {
 	appDir: "_app",
 	appPath: "_app",
-	assets: new Set(["favicon.png","robots.txt"]),
+	assets: new Set([".nojekyll","CNAME","favicon.png","robots.txt"]),
 	mimeTypes: {".png":"image/png",".txt":"text/plain"},
 	_: {
 		entry: {"file":"_app/immutable/start-de1f7a45.js","imports":["_app/immutable/start-de1f7a45.js","_app/immutable/chunks/index-f2c3dd86.js","_app/immutable/chunks/singletons-9109aa86.js","_app/immutable/chunks/parse-c28c2630.js"],"stylesheets":[],"fonts":[]},
